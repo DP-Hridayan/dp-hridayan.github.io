18679dfe3a9bfcf667cb374d91479cbf1545706e
Hridayan <157479796+DP-Hridayan@users.noreply.github.com>
2025-04-16 17:02:47 +0530

    Update typingeffect.js

--- typingeffect.js
+++ typingeffect.js
@@ -1,17 +1,3 @@
-const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-        const body = document.body;
-        
-        const setTheme = () => {
-            if (mediaQuery.matches) {
-                body.classList.add('dark-mode');
-            } else {
-                body.classList.remove('dark-mode');
-            }
-        };
-        
-        mediaQuery.addEventListener('change', setTheme);
-        setTheme();
-
 const quotes = [
     "Crafting Android apps with precision...",
     "Lover of clean code and Material Design...",
