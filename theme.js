af1e28b103ab4d3aedc47cf6cde7eea33e728dac
Hridayan <157479796+DP-Hridayan@users.noreply.github.com>
2025-04-16 17:03:01 +0530

    Update theme.js

--- theme.js
+++ theme.js
@@ -1,1 +1,14 @@
+const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const body = document.body;
+        
+        const setTheme = () => {
+            if (mediaQuery.matches) {
+                body.classList.add('dark-mode');
+            } else {
+                body.classList.remove('dark-mode');
+            }
+        };
+        
+        mediaQuery.addEventListener('change', setTheme);
+        setTheme();
 
